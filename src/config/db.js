10b748044c987ac3a0e8014cd38ac1ade577ae87
Nelson@2025 <gauravcrd@gmail.com>
2024-03-30 11:44:10 +0530

    db commit

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = mongoose
   .createConnection(
-    "mongodb+srv://nelson2025:9hDzNwzJgNPzdwM7@cluster0.pzugxtx.mongodb.net/"
+    "mongodb+srv://nelson2025:9hDzNwzJgNPzdwM7@cluster0.pzugxtx.mongodb.net/selby"
   )
   .on("open", () => {
     console.log("MongoDB Connected");
